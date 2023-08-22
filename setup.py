af1d9d0239947e531b607d8fe09fd2992fde381f
Nagendra Kumar <nagendra.msft@hotmail.com>
2023-08-22 15:23:28 -0700

    modified setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gleenai",
-    version="0.1.0.3",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         'requests',
